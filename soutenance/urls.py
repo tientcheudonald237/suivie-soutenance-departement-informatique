209b492e1114ee2451c84d8279e2272e4213bcc5
geekers-donald237 <tientcheudonald237@gmail.com>
2023-12-13 01:00:09 +0100

    afficheage d;un document
    on recupere deja un document sur une page et on affiche, mais ca marche pas

--- soutenance/urls.py
+++ soutenance/urls.py
@@ -5,4 +5,7 @@ from .import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.login, name='login'),
+    path('login-post', views.login_post, name='login-post'),
+    path('create_document/', views.create_document, name='create_document'),
+    path('view_document/<int:id>/', views.view_document, name='view_document'),
 ]
--- soutenance/views.py
+++ soutenance/views.py
@@ -1,7 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .models import Document
 
 def index(request):
-    return render(request, 'index.html')
-
+    if request.user.is_authenticated:
+        user_documents = Document.objects.filter(user=request.user)
+        context = {'user_documents': user_documents}
+        return render(request, 'index.html', context)
+    else:
+        return render(request, 'index.html')
+    
 def login(request):
     return render(request, 'authentification/login.html')
+
+def login_post(request):
+    if request.method == 'POST':
+        matricule = request.POST.get('matricule')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, matricule=matricule, password=password)
+
+        if user is not None:
+            login(request)
+            return redirect('index') 
+        
+    context = {
+        'ERROR' : 'authentification',
+        'ERROR_MESSAGE' : 'Invalide matricule ou mot de passe'
+    }
+    
+    return render(request,'authentification/login.html', context)
+
+def create_document(request):
+    if request.method == 'POST':
+        document_name = request.POST.get('document_name')
+
+        if request.user.is_authenticated:
+            current_user = request.user    
+            document = Document(title=document_name, user=current_user,category="exemple") 
+            document.save()
+            messages.success(request, 'Document created successfully.')
+        else : 
+            messages.warning(request, 'Erreur Utilisateur n\'ont connecter') 
+        
+        return redirect('index')
+
+    return render(request, 'index.html')
+
+def view_document(request,id):
+    document = get_object_or_404(Document, id=id)
+    user_documents = Document.objects.filter(user=request.user)
+    context = {
+        'document': document,
+        'user_documents': user_documents,
+    }
+    return render(request, 'view_document.html', context)
