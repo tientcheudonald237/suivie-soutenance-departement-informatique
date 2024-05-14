e4b10c8ef092a418b3f38ee2976452c3f7211e26
tientcheudonald237 <103892120+tientcheudonald237@users.noreply.github.com>
2024-05-15 00:27:50 +0100

    commit

--- gestion_soutenaces/settings.py
+++ gestion_soutenaces/settings.py
@@ -131,9 +131,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
+STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
 AUTH_USER_MODEL='soutenance.CustomUser'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_ROOT = BASE_DIR / 'images'
