df89e1e54ae38a5ce01a972ad186f405402bc636
tientcheudonald237 <103892120+tientcheudonald237@users.noreply.github.com>
2024-05-15 21:41:18 +0100

    Update wsgi.py

--- gestion_soutenaces/wsgi.py
+++ gestion_soutenaces/wsgi.py
@@ -1,7 +1,7 @@
 """
 WSGI config for gestion_soutenaces project.
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 
 import os
 
+from django.core.wsgi import get_wsgi_application
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
@@ -16,8 +17,13 @@ import soutenance.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_soutenaces.settings')
 
+application = get_wsgi_application()
+
+app = application
+
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  
+    "https": get_asgi_application(),  
     "websocket": AuthMiddlewareStack(
         URLRouter(
             soutenance.routing.websocket_urlpatterns 
