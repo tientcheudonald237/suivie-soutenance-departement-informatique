4065e369390dca35213e80d01b210626cf22bb61
tientcheudonald237 <103892120+tientcheudonald237@users.noreply.github.com>
2024-05-15 22:06:10 +0100

    modif normal

--- gestion_soutenaces/asgi.py
+++ gestion_soutenaces/asgi.py
@@ -1,16 +1,30 @@
+"""
+ASGI config for gestion_soutenaces project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
 import os
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from soutenance.routing import websocket_urlpatterns
-from channels.auth import AuthMiddlewareStack
+import django 
+from channels.routing import ProtocolTypeRouter 
+from django.core.asgi import get_asgi_application 
+from channels.routing import ProtocolTypeRouter, URLRouter 
+import soutenance.routing
+from channels.auth import AuthMiddlewareStack 
+from channels.security.websocket import AllowedHostsOriginValidator
+from soutenance.consumers import *
+from django.urls import re_path ,path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_soutenaces.settings')
 
-application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket': AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
-    ),
+
+#application = get_asgi_application()
+application = ProtocolTypeRouter({ 
+  'http': get_asgi_application(), 
+  'websocket':AuthMiddlewareStack(URLRouter(soutenance.routing.websocket_urlpatterns)),
 })
--- gestion_soutenaces/wsgi.py
+++ gestion_soutenaces/wsgi.py
@@ -11,9 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import soutenance.routing 
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# import soutenance.routing 
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion_soutenaces.settings')
 
@@ -22,11 +22,13 @@ application = get_wsgi_application()
 app = application
 
 
-application = ProtocolTypeRouter({
-    "https": get_asgi_application(),  
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            soutenance.routing.websocket_urlpatterns 
-        )
-    ),
-})
+
+
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),  
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             soutenance.routing.websocket_urlpatterns 
+#         )
+#     ),
+# })
