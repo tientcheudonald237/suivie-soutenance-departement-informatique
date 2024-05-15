388e1aedb2bda964fbac3ef21367efbf2779bf17
tientcheudonald237 <103892120+tientcheudonald237@users.noreply.github.com>
2024-05-15 21:59:35 +0100

    Update asgi.py

--- gestion_soutenaces/asgi.py
+++ gestion_soutenaces/asgi.py
@@ -1,8 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
 from soutenance.routing import websocket_urlpatterns
 from channels.auth import AuthMiddlewareStack
 
